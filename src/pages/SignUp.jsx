a9459df4b2762cdb1d6c7b979ae9c77a872417b4
yasmin M <yasminm@yasmins-MacBook-Pro.local>
2023-08-18 03:22:30 +0300

    complete SignUp Application

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import RegistrationForm from "../componenets/RegistrationForm";
+
+export default function SignUp() {
+  return (
+    <div className="signUp">
+        <h1>Sign Up</h1>
+      <RegistrationForm />
+    </div>
+  )
+}
